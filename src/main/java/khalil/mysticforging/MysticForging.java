537f9461e5a5a22cb03baa607b204fb006da6ba0
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-02-23 17:47:56 -0500

    Fixed Item Registration -.-

--- src/main/java/khalil/mysticforging/MysticForging.java
+++ src/main/java/khalil/mysticforging/MysticForging.java
@@ -10,17 +10,13 @@ import khalil.mysticforging.registrationhelpers.ModItems;
 
 public class MysticForging implements ModInitializer {
 	public static final String MOD_ID = "mystic-forging";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
