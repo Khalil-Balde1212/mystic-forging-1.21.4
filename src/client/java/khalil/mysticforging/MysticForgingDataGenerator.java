f2a6c8351c9d218061773f821894fe1c78e7e184
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-02-23 11:36:58 -0500

    init commit

---
+++ src/client/java/khalil/mysticforging/MysticForgingDataGenerator.java
@@ -0,0 +1,11 @@
+package khalil.mysticforging;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MysticForgingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
