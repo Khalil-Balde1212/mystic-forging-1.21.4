ecda47197fd3191bb7bfcd0d29e2d13b1f77e485
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-03-02 14:29:42 -0500

    Fixed bug where speed didn't reset when air-pf effect was removed

--- src/main/java/khalil/mysticforging/effects/AirPathfinderEffect.java
+++ src/main/java/khalil/mysticforging/effects/AirPathfinderEffect.java
@@ -4,10 +4,11 @@ import net.minecraft.entity.effect.StatusEffect;
 
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-	
+
 public class AirPathfinderEffect extends StatusEffect {
 
     public AirPathfinderEffect(StatusEffectCategory category, int color) {
@@ -17,8 +18,15 @@ public class AirPathfinderEffect extends StatusEffect {
     @Override
     public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
         if (entity instanceof PlayerEntity player) {
-            //add speed
-            player.getAttributes().getCustomInstance(EntityAttributes.MOVEMENT_SPEED).setBaseValue(0.1 * (amplifier + 1));
+            if (player.isSprinting()) {
+                player.getAttributes()
+                        .getCustomInstance(EntityAttributes.MOVEMENT_SPEED)
+                        .setBaseValue(0.1 * (amplifier + 1));
+            } else {
+                player.getAttributes()
+                        .getCustomInstance(EntityAttributes.MOVEMENT_SPEED)
+                        .setBaseValue(0.1);
+            }
         }
 
         return true;
@@ -27,5 +35,11 @@ public class AirPathfinderEffect extends StatusEffect {
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
+    }
+
+    @Override
+    public void onRemoved(AttributeContainer attributeContainer) {
+        super.onRemoved(attributeContainer);
+        attributeContainer.getCustomInstance(EntityAttributes.MOVEMENT_SPEED).setBaseValue(0.1);
     }
 }
